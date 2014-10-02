873bf58f89d75ff3049f0582dc957201fda81cc9
= <=>
2014-10-02 23:45:05 +0100

    Added high level commentes breaking down the bits of code required

--- src/CompareArrays.java
+++ src/CompareArrays.java
@@ -2,8 +2,35 @@
 public class CompareArrays {
 	public static void main(String[] args)
 	{
-		System.out.println("Values occurring in both arrays: ");
-		System.out.println("Values occurring only in firstArray: ");
-		System.out.println("Values occurring only in secondArray: ");
+		
+		// Constant values should be declared as final and static (e.g. array size, trigger number zero)
+		
+		// Create empty integer array Array1 that will hold up to 100 integers.
+
+		// Create empty integer arrays Array2 that will hold up to 100 integers.
+
+		
+		/* Repeatedly read integer values from the keyboard until the user types "0" (zero, without the quotes).
+		 * Ignore repeated occurrences of a particular value in the data, so check each value to see whether it already occurs in the array before storing it.
+		 * Store those values in the first array Array1
+		 * Array should contain a set (which could be empty) of distinct integer value
+		 * Exclude the terminating zero from array
+		*/
+		
+		
+		/* Repeatedly read integer values from the keyboard until the user types "0" (zero, without the quotes).
+		 * Ignore repeated occurrences of a particular value in the data, so check each value to see whether it already occurs in the array before storing it.
+		 * Store those values in the first array Array2
+		 * Array should contain a set (which could be empty) of distinct integer value
+		 * Exclude the terminating zero from array
+		*/		
+		
+		// Loop through both arrays and print out:
+		System.out.println("Value entered in the first array: ");
+		System.out.println("Value entered in the second array: ");
+		System.out.println("Values occurring in both arrays (intersection): ");
+		System.out.println("Number of intersections: ");
+		System.out.println("Values occurring only in the first array: ");
+		System.out.println("Values occurring only in the second array: ");
 	}
 }
