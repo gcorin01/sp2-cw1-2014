7808928745c0781fbe3a4f81af489e4eecde1420
= <=>
2014-10-02 21:21:12 +0100

    This is the first attempt to save on GitHum the my newly created Java class

---
+++ src/CompareArrays.java
@@ -0,0 +1,9 @@
+
+public class CompareArrays {
+	public static void main(String[] args)
+	{
+		System.out.println("Values occurring in both arrays: ");
+		System.out.println("Values occurring only in firstArray: ");
+		System.out.println("Values occurring only in secondArray: ");
+	}
+}
