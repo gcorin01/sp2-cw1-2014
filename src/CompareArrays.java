2ee428c8a288506adf9f9add1c852bc721144fd1
Gabriel Corinadesi <=>
2014-10-04 16:01:17 +0100

    Trying again to remove the git tags (not sure how the ended up there) from witin the java code

--- src/CompareArrays.java
+++ src/CompareArrays.java
@@ -4,23 +4,11 @@ public class CompareArrays {
 	private static final int ARRAY_SIZE     = 100;
 	private static final int TRIGGER_NUMBER = 0;
 	
-	public static void main(String[] args)
-	{
-<<<<<<< HEAD
+	public static void main(String[] args) {
 		int[] Array1 = new int[ARRAY_SIZE];
 		int[] Array2 = new int[ARRAY_SIZE];
 
-		
 		Scanner input = new Scanner(System.in);
-		
-		
-		// Create empty integer array Array1 that will hold up to 100 integers.
-
-		// Create empty integer arrays Array2 that will hold up to 100 integers.
-
-=======
->>>>>>> 9d86d9d0c1931aa06d8135be7326f1204dd19685
-
 		
 		/* Repeatedly read integer values from the keyboard until the user types "0" (zero, without the quotes).
 		 * Ignore repeated occurrences of a particular value in the data, so check each value to see whether it already occurs in the array before storing it.
