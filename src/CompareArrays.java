a6b37430f126bc8843df317c32dd9b1b11075b2b
Gabriel Corinadesi <=>
2014-10-05 15:45:37 +0100

    Value(s) entered in array 1 and array 2 displayed successfully.

--- src/CompareArrays.java
+++ src/CompareArrays.java
@@ -8,30 +8,17 @@ public class CompareArrays {
     public static void main(String[] args) {
         int[] array1 = new int[ARRAY_SIZE];
         int[] array2 = new int[ARRAY_SIZE];
-        String output = "";
 
-        array1 = fillArray("array1");
-        // array2 = fillArray("array2");
-
-        int arraySize = array1.length;
-        System.out.println("Initial size: " + arraySize); // debug
-        for (int element : array1) {
-
-            if (arraySize != 1) {
-                output = output + element + ", ";
-                arraySize = arraySize--;
-                System.out.println("Loop: " + (arraySize--));  // debug
-            } else {
-                output = output + element;
-            }
-
-        }
+        array1 = fillArray("array 1");
+        array2 = fillArray("array 2");
 
         if (array1.length == 0 && array2.length == 0) {
             System.out.println("Both arrays are empty");
         } else {
-            System.out.println("Value entered in array1: " + output);
-            System.out.println("Value entered in array2: " + array2);
+            System.out.println("Value(s) entered in array 1: "
+                    + getValueEntered(array1));
+            System.out.println("Value(2) entered in array 2: "
+                    + getValueEntered(array2));
             // System.out.println("Values occurring in both arrays (intersection): ");
             // System.out.println("Number of intersections: ");
             // System.out.println("Values occurring only in the first array: ");
@@ -39,31 +26,16 @@ public class CompareArrays {
         }
 
         /*
-         * Repeatedly read integer values from the keyboard until the user types
-         * "0" (zero, without the quotes). Ignore repeated occurrences of a
-         * particular value in the data, so check each value to see whether it
-         * already occurs in the array before storing it. Store those values in
-         * the first array array1 Array should contain a set (which could be
-         * empty) of distinct integer value Exclude the terminating zero from
-         * array
+         * Ignore repeated occurrences of a particular value in the data, so
+         * check each value to see whether it already occurs in the array before
+         * storing it. Store those values in the first array array1 and array2.
+         * Arrays should contain a set (which could be empty) of distinct
+         * integer value Exclude the terminating zero from array
          */
-
-        /*
-         * Repeatedly read integer values from the keyboard until the user types
-         * "0" (zero, without the quotes). Ignore repeated occurrences of a
-         * particular value in the data, so check each value to see whether it
-         * already occurs in the array before storing it. Store those values in
-         * the first array array2 Array should contain a set (which could be
-         * empty) of distinct integer value Exclude the terminating zero from
-         * array
-         */
-
-        // Loop through both arrays and print out:
-
     }
 
-    public static int[] fillArray(String arrayNumber) {
-        int currentSize = 0;
+    // Enter method comment here
+    private static int[] fillArray(String arrayNumber) {
         ArrayList<Integer> tempArray = new ArrayList<Integer>();
 
         Scanner input = new Scanner(System.in);
@@ -74,27 +46,49 @@ public class CompareArrays {
                         + arrayNumber + ":");
 
         int tempInput = input.nextInt();
-        
+
         if (tempInput != TRIGGER_NUMBER) {
             do {
                 tempArray.add(tempInput);
-                System.out.println(currentSize + " " + tempInput);  // debug
-                currentSize++;
-                System.out.println("Enter your next integer:");
+
+                System.out.println("Enter your next integer for " + arrayNumber
+                        + ":");
                 tempInput = input.nextInt();
             } while (tempInput != TRIGGER_NUMBER);
         } else {
-            input.close();            
+            // input.close();
         }
 
-        int[] array = new int[tempArray.size()];
+        int[] outputArray = new int[tempArray.size()];
         int index = 0;
 
         for (int element : tempArray) {
-            array[index] = element;
+            outputArray[index] = element;
             index++;
         }
-        return array;
-        // System.out.println("Both arrays are empty");
+        return outputArray;
+    }
+
+    // Enter method comment here
+    private static String setValueEntered(int[] array) {
+        int arraySize = array.length;
+        String output = "";
+
+        for (int element : array) {
+
+            if (arraySize == 1) {
+                output = output + element;
+            } else {
+                output = output + element + ", ";
+            }
+            arraySize--;
+        }
+        return output;
+    }
+
+    // Enter method comment here
+    private static String getValueEntered(int[] array) {
+        String output = setValueEntered(array);
+        return output;
     }
 }
